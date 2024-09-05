47903a6f33544dda1f6635f2b0b7f9e3a70c8687
Hiten <pethanihiten@gmail.com>
2024-09-05 14:49:53 +0530

    Change code

--- Sources/MyLibraryFirst/MyLibraryFirst.swift
+++ Sources/MyLibraryFirst/MyLibraryFirst.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct MyLibraryFirst{
+    var text = "HI Dear"
+
+    public func HelloToYou() -> String{
+        return "LOL.."
+    }
+
+    public func HelloToYouTO() -> String{
+        return "LOL.."
+    }
+}
